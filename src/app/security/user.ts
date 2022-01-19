526d04d79aa4a137e92e1919724e0ebc93ed0ffb
JoNaulaerts <r0806158@student.thomasmore.be>
2022-01-19 11:40:11 +0100

    authservice gemaakt

---
+++ src/app/security/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  gebruikerId: number,
+  locatieId: number,
+  email: string,
+  password: string,
+  voornaam: string,
+  naam: string,
+  geboortedatum: string,
+  token: string
+}
